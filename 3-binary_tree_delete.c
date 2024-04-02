af7e3d330f12c871a78cce8387ed7b17f1716b06
MayitoPR3 <josemayo3@gmail.com>
2024-04-01 22:59:02 -0400

    push exercise 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - function that deletes an entire binary tree
+ * @tree: pointer to the root node of the tree to delete
+ * Return: No return (void function)
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+	if (tree->left != NULL)
+	{
+		binary_tree_delete(tree->left);
+	}
+
+	if (tree->right != NULL)
+	{
+		binary_tree_delete(tree->right);
+	}
+
+	free(tree);
+	return;
+
+}
